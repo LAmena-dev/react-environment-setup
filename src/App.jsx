1eda6db51870bcff630214416a4eb303120dddd7
LAmena-dev <amena2laremm@gmail.com>
2026-01-10 21:04:47 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
+import { FaveFood, Greeting } from "./gettingStarted/reactComps";
+import { HTMLConv, TodoList } from "./gettingStarted/whatIsJSX";
+import { Animals } from "./gettingStarted/renderTechs";
+import DrinkList from "./gettingStarted/condPrac";
+import { List, Poem, RecipeList } from "./gettingStarted/renderList";
+import {
+  FeedbackForm,
+  FeedbackForm2,
+  Form,
+  Gallery,
+} from "./statesAndEffects/IntroToState";
+import { Person } from "./statesAndEffects/moreState";
+import Clock from "./statesAndEffects/dealWithSideEffects";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      {/* <Greeting />
+    <FaveFood />
+    <HTMLConv />
+    <Bio />
+    <TodoList />
+    <Animals />
+    <DrinkList />
+    <List />
+    <RecipeList />
+    <Poem /> 
+    <Gallery />
+    <Profile />
+    <Prof />
+    <Gallery />
+    <Form />
+    <FeedbackForm />
+    <FeedbackForm2 />
+    <Person />*/}
+
+      <Clock />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { FaveFood, Greeting } from "./gettingStarted/reactComps";
-import { HTMLConv, TodoList } from "./gettingStarted/whatIsJSX";
-import { Animals } from "./gettingStarted/renderTechs";
-import DrinkList from "./gettingStarted/condPrac";
-import { List, Poem, RecipeList } from "./gettingStarted/renderList";
-import {
-  FeedbackForm,
-  FeedbackForm2,
-  Form,
-  Gallery,
-} from "./statesAndEffects/IntroToState";
-import { Person } from "./statesAndEffects/moreState";
+import App from "./App";
 
 // import { Gallery, Prof, Profile } from "./gettingStarted/passProps";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Greeting />
-    <FaveFood />
-    <HTMLConv />
-    <Bio />
-    <TodoList />
-    <Animals />
-    <DrinkList />
-    <List />
-    <RecipeList />
-    <Poem /> 
-    <Gallery />
-    <Profile />
-    <Prof />*/}
-
-    <Gallery />
-    <Form />
-    <FeedbackForm />
-    <FeedbackForm2 />
-    <Person />
+    <App />
   </StrictMode>
 );
